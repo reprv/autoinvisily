715a740449847339a01af541e46ee6c7d26886d9
reprv <reportingrevamp@gmail.com>
2019-07-29 15:30:01 +0500

    My first commit

---
+++ hsetup.py
@@ -0,0 +1,7 @@
+from cx_Freeze import setup, Executable
+
+
+setup(name = "FBAdManager" ,
+      version = "1.2" ,
+      description = "FB Ad Manager" ,
+      executables = [Executable("C:\Python\FBAdManager.py")])
